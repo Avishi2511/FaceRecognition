515b08e44dcab1a6dfe8b6aa9d30753e6e20a4ed
Avishi2511 <avishimittal2004@gmail.com>
2024-02-10 17:34:43 +0530

    final

--- faceDetection.py
+++ faceDetection.py
@@ -1,6 +1,6 @@
 import cv2
 
-face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 """
 img=cv2.imread('test.jpg')
@@ -14,19 +14,20 @@ cv2.imshow('img',img)
 cv2.waitKey()
 """
 
-cap=cv2.VideoCapture('test1.mp4')
+cap=cv2.VideoCapture(0)
 
 while cap.isOpened():
-    _, img=cap.read()
-    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=face_cascade.detectMultiScale(gray,1.1,4)
 
     for(x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-    cv2.imshow('img',img)
+    cv2.imshow('img',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    cv2.waitKey()
+    #cv2.waitKey()
 
 cap.release()
+cap.destroyAllWindows()
